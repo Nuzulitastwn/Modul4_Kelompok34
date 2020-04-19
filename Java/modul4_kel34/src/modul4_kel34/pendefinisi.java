79dff624672d6fc6cc96208b07d6c6f04a29cd03
Nuzulita Setiawati <nuzulitastwn@gmail.com>
2020-04-19 14:37:23 +0700

    Praktikum Modul 4

---
+++ Java/modul4_kel34/src/modul4_kel34/pendefinisi.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul4_kel34;
+
+/**
+ *
+ * @author USER
+ */
+class pendefinisi {
+
+    void greeting() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void kelompok(String kelompok_xx) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String kenalan(String erika, String baca_Novel) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
